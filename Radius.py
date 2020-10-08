2dc5d90803e4a82eb06f5d283d0f954ef468024d
Akash-coder-specs <67906323+Akash-coder-specs@users.noreply.github.com>
2020-10-08 11:57:35 +0100

    This is the python program to fin radius.

---
+++ Radius.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 11:16:47 2020
+
+@author: AKASH
+"""
+a=float(input("Enter the radius of the circle: "))
+print(f"The area of the circle with radius {a} is:",3.14*a**2)
+
